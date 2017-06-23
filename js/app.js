3c0a40bc3c360e23f483f700dd9e619b640ec984
Michael Nagy <michaelnt8@gmail.com>
2017-06-23 16:39:53 -0300

    testing opengraph

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 // generates a random number
 function randPoke () {
-  return Math.floor(Math.random() * 811) + 1
+  return Math.floor(Math.random() * 152) + 1
 }
 // Gets a random pokemon from the pokeapi.co
 function getPoke (params) {
@@ -11,8 +11,19 @@ function getPoke (params) {
   })
   .then(function(json) {
     console.log(json)
+    fetchHTML(json)
   }).catch(function(error) {
     console.log('request failed', error)
   })
 }
+// Fetch the pokemon
 getPoke()
+
+// Let's fetch the OpenGraph and the HTML tags
+function fetchHTML(json) {
+  $('.card-title, .pokemon-name').text(json.name)
+  $(".card-img-top").attr("src", 'https://img.pokemondb.net/artwork/'+json.name+'.jpg')
+  $("meta[property='og:title']").attr('content',json.name)
+  // $( ".card-text" ).load( "https://pokemondb.net/pokedex/"+json.name+" #projects li" );
+}
+ 
